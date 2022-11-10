e00448675f6b8963e8202f585f32d6349cfb22b4
Sergey Savrukhin <savra.sv@yandex.ru>
2022-11-10 18:40:50 +0300

    init commit

---
+++ src/main/java/com/hvdbs/App.java
@@ -0,0 +1,29 @@
+package com.hvdbs;
+
+import com.hvdbs.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit();
+        }
+    }
+}
